bf2032b5387cfd270402883af3dfd6a02595cf91
IBEMBEJOASH <ibembejoash9@gmail.com>
2026-04-14 12:11:30 +0300

    Adding a Top Bar with a Logo Image

--- app/src/main/java/com/ndejje/momocalc/Typography.kt
+++ app/src/main/java/com/ndejje/momocalc/Typography.kt
@@ -1,4 +1,45 @@
 package com.ndejje.momocalc
 
-class Typography {
-}
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+
+
+
+
+
+val PlayfairDisplay = FontFamily(
+    Font(R.font.playfair_display_regular, FontWeight.Normal),
+    Font(R.font.playfair_display_bold, FontWeight.Bold)
+)
+
+val SourceSans3 = FontFamily(
+    Font(R.font.source_sans3_regular, FontWeight.Normal),
+    Font(R.font.source_sans3_semibold, FontWeight.SemiBold)
+)
+
+val MomoTypography = Typography (
+    headlineMedium = TextStyle(
+        fontFamily = PlayfairDisplay,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp
+),
+    bodyLarge = TextStyle (
+        fontFamily = SourceSans3,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+        bodyMedium = TextStyle (
+        fontFamily = SourceSans3,
+        fontWeight = FontWeight.Normal,
+            fontSize = 14.sp
+        ),
+    bodySmall = TextStyle (
+        fontFamily = SourceSans3,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+)
--- app/src/main/java/com/ndejje/momocalc/ui/theme/Typography.kt
+++ app/src/main/java/com/ndejje/momocalc/ui/theme/Typography.kt
@@ -1,4 +1,15 @@
 package com.ndejje.momocalc.ui.theme
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.ndejje.momocalc.R
 
-class Typography {
-}
+val PlayfairDisplay = FontFamily(
+    Font(R.font.playfair_display_regular, FontWeight.Normal),
+    Font(R.font.playfair_display_bold, FontWeight.Bold)
+)
+
+val SourceSans3 = FontFamily(
+    Font(R.font.source_sans3_regular, FontWeight.Normal),
+    Font(R.font.source_sans3_semibold, FontWeight.SemiBold)
+)
